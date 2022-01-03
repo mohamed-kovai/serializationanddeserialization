c3f62271b3399b256fcdb181703c6effd2bafaa3
moli-debugger <mkmohamedalimoli@gmail.com>
2022-01-03 15:45:24 +0530

    Add project files.

---
+++ serializationanddeserialization/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace serializationanddeserialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         //serialization.
+            writeXml();
+        }
+
+        public static void writeXml()
+        {
+
+            Employee emp = new Employee();
+            emp.Id = 1001;
+            emp.Name = "Charle";
+            emp.JobRole = "RD Engeener";
+            XmlSerializer m = new XmlSerializer(typeof(Employee));
+            //XmlSerializer serializer = new XmlSerializer(typeof(Employee));
+            string filePath = @"D:\DotNet\console\serializationanddeserialization\xml\xml1.xml";
+            FileStream file = File.Create(filePath);
+
+            m.Serialize(file,emp);
+            file.Close();
+
+        }
+    }
+
+    public class Employee
+    {
+        public int Id;
+        public string Name;
+        public string JobRole;
+
+       
+    }
+}
